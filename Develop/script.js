8d3af844a3156ab61cdd161feb1d9eadf16aa4b7
Xander Rapstine <xanderrapstine@gmail.com>
2020-02-13 15:07:10 -0600

    Add starter code

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
