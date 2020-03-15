f69819b275760fba8c1d00ab2038aa0a87410309
Xandromus <xanderrapstine@gmail.com>
2020-03-15 17:48:23 -0500

    update instructions

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,7 @@
-// Assignment Code
+// Assignment code here
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
