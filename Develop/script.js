186a66f59e960a9e801f9c76777ac4a8a5c64042
unknown <nickrizzo77277@gmail.com>
2021-05-05 19:22:29 -0600

    added char length prompt and validation

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,16 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var special = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 
+function generatePassword() {
+  //Asking password length 
+  var passwordLength = parseInt(prompt("Please enter number of charactors for password."));
+  // console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter between 8 and 128 characters.");
+    passwordLength = parseInt(prompt("Please enter number of charactors for password."));
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
