1c215cb3ca3ad08176d537d759929e1eaa878485
Cartrell KIDD <cartrellk56@gmail.com>
2021-09-11 13:05:32 -0400

    modal complete, working on functionality

--- Develop/script.js
+++ Develop/script.js
@@ -18,34 +18,36 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-  // Get the modal
-var modal = document.getElementById("myModal");
+    // Get the modal
+  var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("generate");
+  // Get the button that opens the modal
+  var btn = document.getElementById("generate");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     modal.style.display = "none";
   }
-}
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
 
+function test(event){
+  console.log(event);
+}
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
