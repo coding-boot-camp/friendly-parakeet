0bb396cb4660c6f38df89ae7ff9f5068531a607a
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-03 14:31:16 -0600

    length of characters prompt added

--- Develop/script.js
+++ Develop/script.js
@@ -18,11 +18,49 @@ function passwordSelect() {
   }
 passwordSelect();
 
-// add selections to by creating a string of numbers, another of letters , special characters
-// letterABC created to pull individual letters from string - join them latter
-let lettersABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// add window prompt to ask if the user wants numbers generated
+function numberSelect() {
+  var selection = window.prompt("Would you like to include numbers to your random password?"); 
+  if (selection === 'yes' || selection === 'YES') {
+    return getRandomIntInclusive();
+  } else if (selection === 'no' || selection === ' ' || selection === 'NO') {
+    
+  }
+}
+ numberSelect();
 
-// not sure if numbers123 is gonna produce the correct value I am looking for 
+// add window prompt to ask if the user wants numbers generated
+
+// add selections to by creating a string of numbers, another of letters , special characters
+
+// add a function to generate a random letter from the letterABC string
+
+function getRandomString(length) {
+  // letterABC created to pull individual letters from string - join them latter
+  var lettersABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var letter = '';
+  for ( var i = 0; i < length; i++) {
+    letter += lettersABC.charAt(Math.floor(Math.random() * lettersABC.length));
+  }
+  
+  return letter;
+  
+}
+console.log(getRandomString(length));
+
+
+
+
+
+let charactersRan = [ '! " # $ % & () * + , - . / : ; < = > ? @  \ ^ _ ` | '];
+
+
+//window.prompt to ask user how many characters their password should be
+var characterSelect = window.prompt("How long would you like your generated password to be?? Please enter a number between 8 and no more than 128 characters")
+  if (characterSelect => 8 || characterSelect <= 128 ) {
+    window.alert("You chose for your password to be " + characterSelect + " characters long.");
+  }
+
 //function to generate a random number value between 0 and nine -- use function later to call it and join multiple together. 
 
 function getRandomIntInclusive(min, max) {
@@ -30,6 +68,10 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(9);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+// function succesfully calls random number between 0 and 9
+console.log(getRandomIntInclusive());
+
+
 // let numbers123 = getRandomIntInclusive();
 
 
