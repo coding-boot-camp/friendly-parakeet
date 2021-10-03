1b08883286290c15fd6860cc692d28a84821bae1
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-02 22:08:58 -0600

    window alert and window confirm added

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,27 @@
 // Assignment code here
+// * window.alert to create a window alert to notify user to select password criteria to begin
+window.alert("To generate a password, begin by adjusting the criteria needed for the password");
+// generate multiple prompts to confirm if user wants different special characters
+window.confirm("Select below from dropdown list below");
+
+function passwordSelect() {
+  var selection = prompt("Would you like to generate a password?");
+    if (selection != 'yes') {
+      console.log(selection);
+    }
+  }
 
 
-// Get references to the #generate element
+
+// Get references to the #generate element in the html doc in the card-footer class 
 var generateBtn = document.querySelector("#generate");
+
+// create function to generate random password with switch case for adding numbers, special characters, lowercase, uppercase mixed case, etc
+
 
 // Write password to the #password input
 function writePassword() {
+  // the variable password is linked to the textarea tag to generate a new password 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -17,4 +33,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 console.log(password);
+
--- README.md
+++ README.md
@@ -22,7 +22,7 @@ WHEN I click the button to generate a password
 
 THEN I am presented with a series of prompts for password criteria
 
-WHEN prompted for password criteria
+WHEN prompted for password criteria✔️
 
 THEN I select which criteria to include in the password
 
