4d0c0b7ffa4241cf1c95d11c1a580b9364759ad6
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-02 22:23:36 -0600

    created function that prompts user to select criteria

--- Develop/script.js
+++ Develop/script.js
@@ -2,14 +2,21 @@
 // * window.alert to create a window alert to notify user to select password criteria to begin
 window.alert("To generate a password, begin by adjusting the criteria needed for the password");
 // generate multiple prompts to confirm if user wants different special characters
-window.confirm("Select below from dropdown list below");
+window.confirm("Select password options from prompts or choose no in next prompt to select from dropdown list below");
 
+
+// function created to prompt user to make selections in a prompt window or a dropdown list
 function passwordSelect() {
-  var selection = prompt("Would you like to generate a password?");
-    if (selection != 'yes') {
-      console.log(selection);
+  var selection = window.prompt("Would you like to generate a password?");
+    if (selection === 'yes') {
+      // console.log(selection);
+    }else if (selection === 'no') {
+      window.alert("Choose from dropdown list below to determine required password criteria")
+    } else {
+      window.alert("Error, please try again! Please enter either yes or no to continue")
     }
   }
+passwordSelect();
 
 
 
@@ -23,6 +30,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   // the variable password is linked to the textarea tag to generate a new password 
   var password = generatePassword();
+      //assign a value to the 
   var passwordText = document.querySelector("#password");
 
   generatePassword = Math.random();
