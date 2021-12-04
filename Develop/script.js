354cb4c6df23cbbdb8ee890682c161c1a4330d50
Taylor Holmes <taylorh2343@gmail.com>
2021-12-03 21:10:47 -0600

    added numbers,letters, symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,31 @@
 // Assignment code here
 
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var numbers = "0123456789";
+
+var symbols = "!@#$%^&*_-+=";
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//my code
+  
+  
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
