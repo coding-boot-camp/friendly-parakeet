6c127a263ec9731d0b2d6b47305b7dc34559fdf7
rhodemc <rhodemc@gmail.com>
2023-02-08 21:41:11 -0800

    Updated paths and js code

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,24 @@
 // Assignment code here
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+var specialChar = ['!', '\"', '#', '$', '%', '&', '\'', '()', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',];
+
+var numbersMessage = 'Do you want to include numbers in your password?';
+var lettersMessage = 'Do you want to include letters in your password?';
+var specialCharMessage = 'Do you want to include special characters in your password?';
+
+document.getElementById('generate').onclick = startGenerating;
+
+console.log(Math.random() * numbers * letters * specialChar);
+
+var startGenerating = function() {
+  var userChoice = window.prompt(numbersMessage);
+  if (!userChoice) {
+    return;
+  }
+};
+
+startGenerating();
 
 
 // Get references to the #generate element
