dbcaa7df9fa9d203b1631051ce29adab053f3a1d
Amy Kate <amykateoc6@gmail.com>
2023-02-17 23:32:45 -0500

    notes for tutor session

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -9,9 +8,22 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//is there a reason why when we define variables in JS that we don't use the same word that we used for the class?
+
+
+//
+// var password = '';
+// for (var i=0; i< num; i++) {
+//  password += writePassword();
+//}
+// return password;
