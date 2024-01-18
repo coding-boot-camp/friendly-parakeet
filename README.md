5dce301b86034d73a7515a2d1e359f3f201feb0e
Nicodemus <pronic@live.com>
2024-01-17 22:33:56 -0600

    changes to JS on the Password generator
    added code to js a generate an 8 character password

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Password Generator Starter Code
+
---
+++ script.js
@@ -0,0 +1,26 @@
+
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:\'",.<>/?`~';
+
+  let passwordLength = 8;
+  let password = '';
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+
+  return password;
+}
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
